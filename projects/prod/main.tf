5d7f23f4682e47dafd0a077b951cb80698913d9e
PabloBerrettoni <pablo_berrettoni@hotmail.com>
2025-06-10 04:58:32 -0300

    updating secret for PR planning and updating provider for both PR and local env terraform use

--- projects/prod/main.tf
+++ projects/prod/main.tf
@@ -15,7 +15,8 @@ provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  private_key      = var.private_key != "" ? var.private_key : null
+  private_key_path = var.private_key_path != "" ? var.private_key_path : null
   region           = var.region
 }
 
