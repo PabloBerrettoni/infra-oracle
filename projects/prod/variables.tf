c9e9706f70ca84433120ba20cfc3222d60eb8caf
PabloBerrettoni <pablo_berrettoni@hotmail.com>
2025-06-10 17:14:06 -0300

    updating oracle region for resource limitations on sao paulo

--- projects/prod/variables.tf
+++ projects/prod/variables.tf
@@ -31,7 +31,7 @@ variable "private_key_path" {
 variable "region" {
   description = "OCI region"
   type        = string
-  default     = "sa-saopaulo-1"
+  default     = "us-ashburn-1"
 }
 
 # SSH keys
